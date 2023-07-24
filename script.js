c7d2bb07a73f956c06a25bd1c104a5e2c1e28c4f
Leon <leonsarko7@gmail.com>
2023-07-24 16:14:43 +0200

    Added html elements, style and javascript.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function getComputerChoice(){
     let randomNumber = Math.random() * 3;
     randomNumber = Math.floor(randomNumber);
@@ -11,49 +12,44 @@ function getComputerChoice(){
     }    
 }
 
-
-
 //returns 0 if tie, 1 if computer wins, 2 if player wins
 
-function playRound(playerSelection, computerSelection){
-    if (playerSelection === computerSelection){
+function playRound(playerC, computerC){
+
+    if (playerC === computerC){
         return 0;
         //return "Its a tie !";
     }
-    else if (playerSelection === "paper"){
-        if (computerSelection === "scissors"){
+    else if (playerC === "paper"){
+        if (computerC === "scissors"){
             return 1;
             //return "You lose! Scissors beat paper";
         }
-        else if (computerSelection === "rock"){
+        else if (computerC === "rock"){
             return 2;
             //return "You win! Paper beats rock";
         }    
     }
-    else if (playerSelection === "scissors"){
-        if (computerSelection === "paper"){
+    else if (playerC=== "scissors"){
+        if (computerC === "paper"){
             return 2;
             //return "You win! Scissors beat paper";
         }
-        else if (computerSelection === "rock"){
+        else if (computerC=== "rock"){
             return 1;
             //return "You lose! Rock beats scissors";
         }    
     }
-    else if (playerSelection === "rock"){
-        if (computerSelection === "paper"){
+    else if (playerC === "rock"){
+        if (computerC === "paper"){
             return 1;
            // return "You lose! Paper beats rock";
         }
-        else if (computerSelection === "scissors"){
+        else if (computerC=== "scissors"){
             return 2;
             //return "You win! Rock beats scissors";
         }    
     }
-    else{
-        return "You did not input a valid option. Try again!";
-    }
-    
 }
 
 
@@ -69,34 +65,53 @@ function game(){
     let playerWin = 0;
     let computerWin = 0;
 
-    for (let i = 0; i < 5; i++){
-        let result = playRound(playerSelection(), getComputerChoice());
-        if (result === 0){
-            continue;
-        }
-        if (result === 1){
-            computerWin++;
-            continue;
-        }
-        if (result === 2){
-            playerWin++;
-            continue;
-        }
-        
-        console.log(playerWin);
-        console.log(computerWin);
-    }
+    const humanScore = document.getElementById("human-score");
+    const alienScore = document.getElementById("alien-score");
+    const buttons = document.querySelectorAll("button");
+    const winner = document.querySelector(".winner");
 
-    if (playerWin > computerWin){
-        console.log("Player wins");
-    }else if (playerWin < computerWin){
-        console.log("Computer wins");
-
-    }else{
-        console.log("It's a tie!");
-    }
+    buttons.forEach(button => {
+        button.addEventListener("click", (e) =>{
+           playerC = e.target.parentElement.id;
+           computerC = getComputerChoice();
 
 
+           if (playRound(playerC, computerC) == 0){
+            winner.innerHTML = "Its a tie! You chose " + `${playerC.toUpperCase()}` + " and the Alien chose " + `${computerC.toUpperCase()}`; 
+           }
+           if (playRound(playerC, computerC) == 1){
+               computerWin++;
+               alienScore.textContent = "Alien: " + `${computerWin}`;
+               winner.innerHTML = "You lost! You chose " + `${playerC.toUpperCase()}` + " and the Alien chose " + `${computerC.toUpperCase()}`; 
+   
+           }
+   
+           if (playRound(playerC, computerC) == 2){
+               playerWin++;
+               humanScore.textContent = "Player: " + `${playerWin}`;
+               winner.innerHTML = "You won! You chose " + `${playerC.toUpperCase()}` + " and the Alien chose " + `${computerC.toUpperCase()}`; 
+            } 
+
+
+           if (playerWin == 5){
+            winner.innerHTML = "You won. Human race is saved !";
+            playerWin = 0;
+            computerWin = 0;
+            alienScore.textContent = "Alien: " + `${computerWin}`;
+            humanScore.textContent = "Player: " + `${playerWin}`;
+            }
+
+            if (computerWin == 5){
+                winner.innerHTML = "You lost. Aliens invade and destroy Earth !";
+                playerWin = 0;
+                computerWin = 0;
+                alienScore.textContent ="Alien: " + `${computerWin}`;
+                humanScore.textContent ="Player: " + `${playerWin}`;
+            }
+   
+        });
+   
+    })
 
 
 }
